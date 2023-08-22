a47274419f19930f859b7d7535f6812b999bbc70
Aurélien <au.re@live.com>
2023-08-22 23:17:47 +0200

    migrate util function

--- packages/@pufflig/ps-chains/src/index.ts
+++ packages/@pufflig/ps-chains/src/index.ts
@@ -1,4 +1,3 @@
 export { runFromNode } from "./engines/dataflow/runFromNode";
 export { updateNodeInput } from "./engines/dataflow/updateNodeInput";
 export * from "./types";
-export { chainToReactFlow } from "./utils/chainToReactFlow";
