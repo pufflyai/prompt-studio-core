5f2d62930d6a0bf08d0ce3efa6572d2a20af01ca
Aurélien <au.re@live.com>
2023-09-23 15:36:24 +0200

    fix provider export

--- packages/@pufflig/ps-models/src/index.ts
+++ packages/@pufflig/ps-models/src/index.ts
@@ -6,17 +6,37 @@ export const chat_models = { ...openai_chat };
 export const completion_models = { ...openai_completion, ...open_router_completion, ...hf_completion };
 export const embedding_models = { ...openai_embedding };
 export const models = { ...openai_chat, ...openai_completion, ...open_router_completion, ...hf_completion };
-export const settings = { ...openai_settings, ...hf_settings, ...open_router_settings };
 
+// provider settings
+export const settings = [...openai_settings, ...hf_settings, ...open_router_settings];
+export const providers = [
+  {
+    name: "OpenAI",
+    description: "OpenAI API",
+    settings: openai_settings,
+  },
+  {
+    name: "HuggingFace",
+    description: "",
+    settings: hf_settings,
+  },
+  {
+    name: "Open Router",
+    description: "",
+    settings: open_router_settings,
+  },
+];
+
+// export globals variables
+export { OPENAI_API_KEY } from "./models/openai";
+export { HF_ACCESS_TOKEN } from "./models/hf";
+export { OPEN_ROUTER_API_KEY } from "./models/open_router";
+
+// openai specific models
 export const openai = {
   chat_models: openai_chat,
   completion_models: openai_completion,
   embedding_models: openai_embedding,
 };
-
-// export globals
-export { OPENAI_API_KEY } from "./models/openai";
-export { HF_ACCESS_TOKEN } from "./models/hf";
-export { OPEN_ROUTER_API_KEY } from "./models/open_router";
 
 export const default_completion_model = "gpt-3.5-turbo-instruct";
