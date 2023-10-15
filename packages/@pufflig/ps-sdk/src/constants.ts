89f5189e4ccf2c1f175a21899f4797e2dd4e1eed
Aurélien <au.re@live.com>
2023-10-15 18:42:51 +0200

    update getServiceURl method

--- packages/@pufflig/ps-sdk/src/constants.ts
+++ packages/@pufflig/ps-sdk/src/constants.ts
@@ -1,8 +1,5 @@
 export const SERVICE_URL = "https://api.prompt.studio/api/v1/completion";
 
 export const getServiceUrl = () => {
-  if (process.env.PROMPT_STUDIO_RUNTIME === "local") {
-    return "http://localhost:8088/api/v1/completion";
-  }
-  return SERVICE_URL;
+  return process.env.PROMPT_STUDIO_SERVICE_URL || SERVICE_URL;
 };
