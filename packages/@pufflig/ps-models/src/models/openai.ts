eddd45a8756db270554279257bb84359ee5e8cb4
Aurelien Franky <au.re@live.com>
2023-11-23 11:16:59 +0100

    add gpt-4-turbo

--- packages/@pufflig/ps-models/src/models/openai.ts
+++ packages/@pufflig/ps-models/src/models/openai.ts
@@ -207,6 +207,71 @@ export const openai_completion: ModelDefinition = {
 };
 
 export const openai_chat: ModelDefinition = {
+  "gpt-4-1106-preview": {
+    modelId: "gpt-4-1106-preview",
+    description: "More capable than any GPT-3.5 model, able to do more complex tasks, and optimized for chat.",
+    settings: openai_settings,
+    streaming: true,
+    contextLength: 4096,
+    parameters: [
+      {
+        id: "temperature",
+        type: "number",
+        name: "Temperature",
+        max: 2,
+        min: 0,
+        step: 0.1,
+        defaultValue: 0.4,
+        description:
+          "What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.",
+      },
+      {
+        id: "max_tokens",
+        type: "number",
+        name: "Max Tokens",
+        // although the documentation says the model should support 8192 tokens, it actually supports 4096
+        max: 4096,
+        min: 1,
+        step: 20,
+        defaultValue: 1024,
+        description:
+          "The maximum number of tokens to generate in the completion. The total length of input tokens and generated tokens is limited by the model's context length.",
+      },
+      {
+        id: "top_p",
+        type: "number",
+        name: "Top P",
+        max: 1,
+        min: 0,
+        step: 0.1,
+        defaultValue: 1,
+        description:
+          "An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.",
+      },
+      {
+        id: "frequency_penalty",
+        type: "number",
+        name: "Frequency penalty",
+        max: 2,
+        min: -2,
+        step: 0.1,
+        defaultValue: 0,
+        description:
+          "Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.",
+      },
+      {
+        id: "presence_penalty",
+        type: "number",
+        name: "Presence penalty",
+        max: 2,
+        min: -2,
+        step: 0.1,
+        defaultValue: 0,
+        description:
+          "Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.",
+      },
+    ],
+  },
   "gpt-4": {
     modelId: "gpt-4",
     description: "More capable than any GPT-3.5 model, able to do more complex tasks, and optimized for chat.",
